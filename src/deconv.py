5aaebaf71acf7a07b735219fb83c570e1ea7c186
Jacob Polloreno <makaveliix16@gmail.com>
2017-08-26 12:56:19 -0700

    init

---
+++ src/deconv.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Deconvolution Step
+
+Keras has a layer for deconv Conv2DTranspose from TF
+
+http://www.matthewzeiler.com/wp-content/uploads/2017/07/cvpr2010.pdf
+"""
+
+class Deconv(object):
+
+    def __init__(self, classifier):
+        pass
