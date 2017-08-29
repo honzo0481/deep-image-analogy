75775bd2bb0516e6404997997e5fb429e690f6ac
Jacob Polloreno <jacob.polloreno@gmail.com>
2017-08-28 19:08:01 -0700

    minor code cleanup

--- src/deconv.py
+++ src/deconv.py
@@ -2,6 +2,10 @@
 """
 Deconvolution Step
 
+Only content reconstruction is implemented at the moment.
+
+Models supported are VGG19
+
 """
 
 import keras.backend as K
@@ -11,27 +15,35 @@ import os
 from keras.preprocessing.image import load_img, img_to_array
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.misc import imsave
+from typing import Tuple, List, Dict, Callable
 
 
 class Deconv(object):
 
-    def __init__(self, model, preprocess_input, path_A):
+    def __init__(self, model, preprocess_input: Callable, path_A: str,
+                 target_size: Tuple=(448, 448)) -> None:
         self.model = model
         self.preprocess_input = preprocess_input
         if not os.path.isfile(path_A):
             raise IOError
-        self.path_A = path_A
-        self.img_ncols, self.img_nrows = load_img(path_A).size
+        self.img_ncols, self.img_nrows = target_size
+        self.img_a = self._preprocess_image(path_A)
 
-    def _preprocess_image(self):
-        img = load_img(self.path_A,
-                       target_size=(self.img_ncols, self.img_nrows))
+    def _preprocess_image(self, path: str) -> np.ndarray:
+        """
+        Load img and output input tensor with the target size
+        """
+        img = load_img(path, target_size=(self.img_ncols, self.img_nrows))
         img = img_to_array(img)
         img = np.expand_dims(img, axis=0)
         img = self.preprocess_input(img)
         return img
 
-    def _deprocess_image(self, x):
+    def _deprocess_image(self, x: np.ndarray) -> np.ndarray:
+        """
+        Remove normalizations and necessary changes to output
+        img array
+        """
         if K.image_data_format() == 'channels_first':
             x = x.reshape((3, self.img_ncols, self.img_nrows))
             x = x.transpose((1, 2, 0))
@@ -48,35 +60,62 @@ class Deconv(object):
         x = np.clip(x, 0, 255).astype('uint8')
         return x
 
-    def _content_loss(self, base, gen):
+    def _content_loss(self, base: np.ndarray, gen: np.ndarray) -> np.ndarray:
+        """
+        Loss function for content similiarly between base image and generated
+        image
+
+        By minimizing the loss, our generated image will be consistent with
+        the base image.
+        """
         return K.sum(K.square(gen - base))
 
-    def deconv(self, noise_ratio=1.0, content_weight=1.0, layer='block5_conv1',
-               iterations=15, output_steps=5, save_images=True,
-               save_dir='deconv_results'):
+    def deconv(self, noise_ratio: float=1.0, content_weight: float=1.0,
+               layer: str='block5_conv1', iterations: int=15,
+               output_steps: int=5, save_images: bool=True,
+               save_dir: str='deconv_results') -> np.ndarray:
+        """
+        Performs image deconvolution on a layer in the vgg19 model
 
-        img_a = self._preprocess_image()
+        :param noise_ratio: float,
+        :param content_weight: float,
+        :param layer: str,
+        :param iterations: int,
+        :param output_steps: int,
+        :param save_images: bool,
+        :param save_dir: str,
 
-        a_image = K.variable(img_a)
+        :return array, deprocessed img array after the final iteration of the
+        optimization function
+
+        """
+
+        # Get inputs
+        a_image = K.variable(self.img_a)
         generated_image = K.placeholder((1, self.img_nrows, self.img_ncols, 3))
-
         input_tensor = K.concatenate([a_image,
                                       generated_image], axis=0)
 
+        # Get Model
         model = self.model(include_top=False, weights='imagenet',
                            input_tensor=input_tensor)
 
         feature_maps = dict([(layer.name, layer.output)
                              for layer in model.layers])
 
-        # loss
-        loss = K.variable(0.)
+        if layer not in feature_maps:
+            raise IOError("input layer cannot be found in model")
+
+        # Get desired features
         layer_features = feature_maps[layer]
         a_features = layer_features[0, :, :, :]
         gen_features = layer_features[1, :, :, :]
+
+        # Setup Loss
+        loss = K.variable(0.)
         loss += content_weight * self._content_loss(a_features, gen_features)
 
-        # get the gradients of the generated image wrt the loss
+        # Get the gradients of the generated image wrt the loss
         grads = K.gradients(loss, generated_image)
 
         outputs = [loss]
@@ -93,7 +132,7 @@ class Deconv(object):
         # Generated random noise from content img
         noise_img = np.random.uniform(-20., 20.,
                                       (1, self.img_ncols, self.img_nrows, 3))
-        x = noise_ratio * noise_img + (1. - noise_ratio) * img_a
+        input_img = noise_ratio * noise_img + (1. - noise_ratio) * self.img_a
 
         if save_images:
             if not os.path.exists(save_dir):
@@ -102,40 +141,43 @@ class Deconv(object):
         # run scipy-based optimization (L-BFGS) over the pixels of the
         # generated image so as to minimize the content loss
         for i in range(iterations):
-            x, _, _ = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
-                                    fprime=evaluator.grads, maxfun=20)
-            # save current generated image
+            input_img, _, _ = fmin_l_bfgs_b(evaluator.loss,
+                                            input_img.flatten(),
+                                            fprime=evaluator.grads,
+                                            maxfun=20)
             if i % output_steps == 0:
-                img = self._deprocess_image(x.copy())
+                img = self._deprocess_image(input_img.copy())
+                # save current generated image
                 if save_images:
                     fname = '{}/from_{}_at_{}.png'.format(save_dir, layer, i)
                     imsave(fname, img)
 
-        return self._deprocess_image(x.copy())
+        return self._deprocess_image(input_img.copy())
 
 
 class Evaluator(object):
 
-    def __init__(self, f_outputs):
-        self.loss_value = None
-        self.grads_values = None
-        self._f_outputs = f_outputs
+    def __init__(self, f_outputs: Callable) -> None:
+        self.loss_value: float = None
+        self.grads_values: List = None
+        self._f_outputs: Callable = f_outputs
 
-    def loss(self, x):
+    def loss(self, x: np.ndarray) -> float:
         assert self.loss_value is None
         loss_value, grad_values = self._eval_loss_and_grads(x)
         self.loss_value = loss_value
         self.grad_values = grad_values
         return self.loss_value
 
-    def grads(self, x):
+    def grads(self, x: np.ndarray) -> List:
         assert self.loss_value is not None
         grad_values = np.copy(self.grad_values)
         self.loss_value = None
         self.grad_values = None
         return grad_values
 
-    def _eval_loss_and_grads(self, x, size=(448, 448)):
+    def _eval_loss_and_grads(self, x: np.ndarray,
+                             size: Tuple=(448, 448)) -> Tuple[float, List]:
         if K.image_data_format() == 'channels_first':
             x = x.reshape((1, 3, size[0], size[1]))
         else:
