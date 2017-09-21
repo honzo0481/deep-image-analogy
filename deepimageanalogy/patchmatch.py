c21fe01f22490808b505f5015df40bfefdfaa1cb
honzo0481 <gonzalesre@gmail.com>
2017-09-21 01:05:18 -0700

    stubbed out a Patchmatcher class.

--- deepimageanalogy/patchmatch.py
+++ deepimageanalogy/patchmatch.py
@@ -3,5 +3,33 @@
 import numpy as np
 
 
-def patchmatch(A, B):
-    """Return a NNF that maps from A to B."""
+class Patchmatcher:
+    """compute an NNF that maps patches in A/A' to B/B' or vice versa."""
+
+    def __init__(self, A, Ap, B, Bp, NNF=None):
+        """"instantiate a PatchMatcher object.
+
+        Parameters
+        ----------
+        A: ndarray
+            The original structural image.
+        Ap: ndarray
+            The analogous, synthetic structural image.
+        B: ndarray
+            The analogous, synthetic style image.
+        Bp: ndarray
+            The original style image.
+        NNF: ndarray or None
+            An array containing the offsets. Either the upsampled NNF from the
+            previous layer should be passed in or None. If None, an array with
+            random offsets will be created.
+        """
+
+    def _propagate(self):
+        """Propagate adjacent good offsets."""
+
+    def _random_search(self):
+        """Search for good offsets at exponentially descreasing distances."""
+
+    def get_nnf(self):
+        """Return an nnf."""
