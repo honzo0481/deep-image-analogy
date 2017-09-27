9843c17edf868331646aae19d21571edc20d0dd1
honzo0481 <gonzalesre@gmail.com>
2017-09-26 19:17:20 -0700

    peudo code comments. need to squash.

--- deepimageanalogy/patchmatch.py
+++ deepimageanalogy/patchmatch.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-class Patchmatcher:
+class Patchmatcher(object):
     """compute an NNF that maps patches in A/A' to B/B' or vice versa."""
 
     def __init__(self, A, Ap, B, Bp, patchsize=3, NNF=None):
@@ -28,11 +28,12 @@ class Patchmatcher:
         self.Ap = Ap
         self.B = B
         self.Bp = Bp
+        self.patchsize = patchsize
         self.NNF = NNF or self._random_init()
 
     def _random_init(self):
         """Initialize an NNF filled with random offsets."""
-        # Floor division gives the before and after pad width
+        # Floor division gives the before and after pad widths
         pad_width = self.patchsize // 2
         # for now assume the first two input dims will always be square.
         length = self.A.shape[0] - pad_width*2
@@ -51,10 +52,30 @@ class Patchmatcher:
 
     def _propagate(self):
         """Propagate adjacent good offsets."""
+            # get the offsets f(x-1, y), and f(x, y-1)
+            # compute distance D between:
+            # A[x, y], B[f(x, y)], B[f(x-1, y) + (1, 0)], and B[f(x, y-1) + (0, 1)]
+            # set f[x, y] to be argmin of computed distances
+        # for even iterations iterate in reverse scan order and examine patches
+
 
     def _random_search(self):
         """Search for good offsets at exponentially descreasing distances."""
         # u = v0 + w * a**i * R
+        # v0: current nearest neighbor f(x,y)
+        # w: search radius. default max image dim.
+        # alpha: scaling factor. default 1/2
+        # i: 0, 1, 2, ... until w*a**i < 1
+        # R: uniform random number in [-1, 1], [-1, 1]
 
     def predict(self):
         """Return an nnf."""
+        # repeat 5 times
+        # on odd iterations:
+            # iterate over all coords x,y in NNF from (left to right, top to bottom)
+            # propagate down and right
+            # random search
+        # on odd iterations:
+            # iterate over all coords x,y in NNF from (right to left, bottom to top)
+            # propagate down and right
+            # random search
