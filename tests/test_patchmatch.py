00e0dfa5ca7c1a7bd2e8b6d165e186997dd3391c
honzo0481 <gonzalesre@gmail.com>
2017-10-24 12:58:15 -0700

    renamed fixtures in preparation for adding more. stubbed test for get_patch.

--- tests/test_patchmatch.py
+++ tests/test_patchmatch.py
@@ -14,21 +14,21 @@ def nnf():
 
 
 @pytest.fixture
-def A():
-    A = np.zeros((6, 6, 3))
-    A[:3, 3:, :] += 0.25
-    A[3:, :3, :] += 0.5
-    A[3:, 3:, :] += 1.0
-    return A
+def A6():
+    A6 = np.zeros((6, 6, 3))
+    A6[:3, 3:, :] += 0.25
+    A6[3:, :3, :] += 0.5
+    A6[3:, 3:, :] += 1.0
+    return A6
 
 
 @pytest.fixture
-def Bp():
-    Bp = np.zeros((6, 6, 3))
-    Bp[:3, :3, :] += 1.0
-    Bp[:3, 3:, :] += 0.5
-    Bp[3:, :3, :] += 0.25
-    return Bp
+def Bp6():
+    Bp6 = np.zeros((6, 6, 3))
+    Bp6[:3, :3, :] += 1.0
+    Bp6[:3, 3:, :] += 0.5
+    Bp6[3:, :3, :] += 0.25
+    return Bp6
 
 
 @pytest.mark.skip('Not implemented.')
@@ -46,132 +46,60 @@ def test_patchmatcher_raises_ValueError_when_passed_3_images():
     """Patchmatcher should raise a ValueError if passed 3 images."""
 
 
-def test_random_init_nnf_type(A, Bp):
+def test_random_init_nnf_type(A6, Bp6):
     """Patchmatcher should return a randomly initialized nnf of type ndarray."""
-    pm = Patchmatcher(A, Bp)
+    pm = Patchmatcher(A6, Bp6)
     assert isinstance(pm.NNF, np.ndarray)
 
 
 @pytest.mark.parametrize('patchsize, padwidth', [(3, 1), (5, 2)])
-def test_padwidth(A, Bp, patchsize, padwidth):
+def test_padwidth(A6, Bp6, patchsize, padwidth):
     """Padwith should be half the patchsize rounded down."""
-    pm = Patchmatcher(A, Bp, patchsize=patchsize)
+    pm = Patchmatcher(A6, Bp6, patchsize=patchsize)
     assert pm.padwidth == padwidth
 
 
-def test_nnfwidth(A, Bp):
+def test_nnfwidth(A6, Bp6):
     """nnfwidth should be input shape minus left padding and right padding."""
-    pm = Patchmatcher(A, Bp, patchsize=3)
+    pm = Patchmatcher(A6, Bp6, patchsize=3)
     assert pm.nnfwidth == 4
 
 
-def test_use_precomputed_nnf(A, Bp, nnf):
+def test_use_precomputed_nnf(A6, Bp6, nnf):
     """Patchmatcher should accept a precomputed NNF as a param."""
-    pm = Patchmatcher(A, Bp, NNF=nnf)
+    pm = Patchmatcher(A6, Bp6, NNF=nnf)
     assert np.allclose(nnf, pm.NNF)
 
 
-def test_random_init_nnf_size(A, Bp):
+def test_random_init_nnf_size(A6, Bp6):
     """_random_init should return an nnf with size equal to nnfwidth squared."""
-    pm = Patchmatcher(A, Bp)
+    pm = Patchmatcher(A6, Bp6)
     nnfwidth = pm.nnfwidth
     assert pm.NNF.size == nnfwidth**2
 
 
-@pytest.mark.parametrize('i, o, p', [
-    (0, 2, (0, 0)),
-    (1, 4, (0, 1)),
-    (2, 2, (0, 2)),
-    (3, 11, (0, 3)),
-    (4, 4, (1, 0)),
-    (5, 5, (1, 1)),
-    (6, 15, (1, 2)),
-    (7, 5, (1, 3)),
-    (8, 0, (2, 0)),
-    (9, 3, (2, 1)),
-    (10, 8, (2, 2)),
-    (11, 7, (2, 3)),
-    (12, 2, (3, 0)),
-    (13, 2, (3, 1)),
-    (14, 2, (3, 2)),
-    (15, 15, (3, 3))
+@pytest.mark.parametrize('i, o, p, q0, q1, q2', [
+    (0, 2, (0, 0), (0, 2), (0, 0), (1, 2)),
+    (1, 4, (0, 1), (1, 0), (0, 3), (1, 2)),
+    (2, 2, (0, 2), (0, 2), (1, 1), (1, 2)),
+    (3, 11, (0, 3), (2, 3), (0, 3), (0, 3)),
+    (4, 4, (1, 0), (1, 0), (3, 0), (1, 2)),
+    (5, 5, (1, 1), (1, 1), (1, 1), (2, 0)),
+    (6, 15, (1, 2), (3, 3), (1, 2), (1, 2)),
+    (7, 5, (1, 3), (1, 1), (0, 0), (3, 3)),
+    (8, 0, (2, 0), (0, 0), (1, 2), (2, 0)),
+    (9, 3, (2, 1), (0, 3), (0, 1), (2, 1)),
+    (10, 8, (2, 2), (2, 0), (1, 0), (0, 3)),
+    (11, 7, (2, 3), (1, 3), (2, 1), (2, 1)),
+    (12, 2, (3, 0), (0, 2), (2, 0), (1, 0)),
+    (13, 2, (3, 1), (0, 2), (0, 3), (1, 3)),
+    (14, 2, (3, 2), (0, 2), (0, 3), (3, 0)),
+    (15, 15, (3, 3), (3, 3), (0, 3), (2, 3))
 ])
-def test_get_forward_patches_p(i, o, p, A, Bp, nnf):
-    """_get_patches should return the correct subset of offsets from the nnf."""
-    pm = Patchmatcher(A, Bp, NNF=nnf)
-    assert pm._get_patches(i, o)[0] == p
-
-
-@pytest.mark.parametrize('i, o, q0', [
-    (0, 2, (0, 2)),
-    (1, 4, (1, 0)),
-    (2, 2, (0, 2)),
-    (3, 11, (2, 3)),
-    (4, 4, (1, 0)),
-    (5, 5, (1, 1)),
-    (6, 15, (3, 3)),
-    (7, 5, (1, 1)),
-    (8, 0, (0, 0)),
-    (9, 3, (0, 3)),
-    (10, 8, (2, 0)),
-    (11, 7, (1, 3)),
-    (12, 2, (0, 2)),
-    (13, 2, (0, 2)),
-    (14, 2, (0, 2)),
-    (15, 15, (3, 3))
-])
-def test_get_forward_patches_q0(i, o, q0, A, Bp, nnf):
-    """_get_patches should return the correct subset of offsets from the nnf."""
-    pm = Patchmatcher(A, Bp, NNF=nnf)
-    assert pm._get_patches(i, o)[1] == q0
-
-
-@pytest.mark.parametrize('i, o, q1', [
-    (0, 2, (0, 0)),
-    (1, 4, (0, 3)),
-    (2, 2, (1, 1)),
-    (3, 11, (0, 3)),
-    (4, 4, (3, 0)),
-    (5, 5, (1, 1)),
-    (6, 15, (1, 2)),
-    (7, 5, (0, 0)),
-    (8, 0, (1, 2)),
-    (9, 3, (0, 1)),
-    (10, 8, (1, 0)),
-    (11, 7, (2, 1)),
-    (12, 2, (2, 0)),
-    (13, 2, (0, 3)),
-    (14, 2, (0, 3)),
-    (15, 15, (0, 3))
-])
-def test_get_forward_patches_q1(i, o, q1, A, Bp, nnf):
-    """_get_patches should return the correct subset of offsets from the nnf."""
-    pm = Patchmatcher(A, Bp, NNF=nnf)
-    assert pm._get_patches(i, o)[2] == q1
-
-
-@pytest.mark.parametrize('i, o, q2', [
-    (0, 2, (1, 2)),
-    (1, 4, (1, 2)),
-    (2, 2, (1, 2)),
-    (3, 11, (0, 3)),
-    (4, 4, (1, 2)),
-    (5, 5, (2, 0)),
-    (6, 15, (1, 2)),
-    (7, 5, (3, 3)),
-    (8, 0, (2, 0)),
-    (9, 3, (2, 1)),
-    (10, 8, (0, 3)),
-    (11, 7, (2, 1)),
-    (12, 2, (1, 0)),
-    (13, 2, (1, 3)),
-    (14, 2, (3, 0)),
-    (15, 15, (2, 3))
-])
-def test_get_forward_patches_q2(i, o, q2, A, Bp, nnf):
-    """_get_patches should return the correct subset of offsets from the nnf."""
-    pm = Patchmatcher(A, Bp, NNF=nnf)
-    assert pm._get_patches(i, o)[3] == q2
+def test_get_patche_scan_order(i, o, p, q0, q1, q2, A6, Bp6, nnf):
+    """In scan order _get_patch_centers returns offset plus neighbors below and left."""
+    pm = Patchmatcher(A6, Bp6, NNF=nnf)
+    assert pm._get_patch_centers(i, o) == (p, q0, q1, q2)
 
 
 @pytest.mark.parametrize('i, o, p, q0, q1, q2', [
@@ -192,10 +120,10 @@ def test_get_forward_patches_q2(i, o, q2, A, Bp, nnf):
     (1, 4, (0, 1), (1, 0), (0, 1), (0, 1)),
     (0, 2, (0, 0), (0, 2), (0, 3), (0, 0))
 ])
-def test_get_patches_reverse_scan_order(i, o, p, q0, q1, q2, A, Bp, nnf):
-    """In reverse scan order _get_patches should return an offset plus its neighbors above and right."""
-    pm = Patchmatcher(A, Bp, NNF=nnf)
-    assert pm._get_patches(i, o, scan_order=False) == (p, q0, q1, q2)
+def test_get_patch_centers_reverse_scan_order(i, o, p, q0, q1, q2, A6, Bp6, nnf):
+    """In reverse scan order _get_patch_centers returns offset plus neighbors above and right."""
+    pm = Patchmatcher(A6, Bp6, NNF=nnf)
+    assert pm._get_patch_centers(i, o, scan_order=False) == (p, q0, q1, q2)
 
 
 @pytest.mark.skip('Not implemented.')
@@ -222,3 +150,7 @@ def test_random_search_gets_patches_at_exponentially_decreasing_dist():
 def test_predict():
     """"""
 
+
+@pytest.mark.skip('Not implemented.')
+def test_get_patch():
+    """"""
