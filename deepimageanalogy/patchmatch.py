3f16550d6f2df8db0c8f891a8cf6819b860ecf4d
honzo0481 <gonzalesre@gmail.com>
2017-10-24 12:59:09 -0700

    renamed _get_patches to _get_patch_centers, added _get_patch method, moved dist metrics back to global scope.

--- deepimageanalogy/patchmatch.py
+++ deepimageanalogy/patchmatch.py
@@ -2,7 +2,57 @@
 
 import numpy as np
 
+def  bidirectional_distance(P0, Q0, P1, Q1):
+    """Return the bidirectionally weighted distance D between patches P and Q.
 
+    Parameters
+    ----------
+    p: 
+
+    q: 
+
+    A: ndarray
+        The original structural image.
+    Ap: ndarray
+        The analogous, synthetic structural image.
+    B: ndarray
+        The analogous, synthetic style image.
+    Bp: ndarray
+        The original style image.
+
+    Returns
+    ------
+    d: float32
+        the computed distance.
+    """
+
+def unidirectional_distance(P, Q):
+    """Return the unidirectional distance D between patches P and Q.
+
+    Where p and q are patch centers in the source and target images respectively.
+
+    Parameters
+    ----------
+    p: patch center from image A
+
+    q: patch center from image Bp
+
+    A: ndarray
+        The original structural image.
+    Bp: ndarray
+        The original style image.
+
+    Returns
+    ------
+    d: float32
+        the computed distance.
+    """
+    Fbar = lambda mat: mat / np.linalg.det(mat)
+
+    fbar_x = Fbar(P)
+    fbar_y = Fbar(Q)
+
+    return np.linalg.norm(fbar_x - fbar_y)**2
 
 
 class Patchmatcher(object):
@@ -81,7 +131,7 @@ class Patchmatcher(object):
 
         return NNF
 
-    def _get_patches(self, index, offset, scan_order=True):
+    def _get_patch_centers(self, index, offset, scan_order=True):
         """"""
         w = self.nnfwidth
         s = len(self.NNF)
@@ -110,15 +160,23 @@ class Patchmatcher(object):
 
         return p, q0, q1, q2
 
+    def _get_patch(self, coord, mat):
+        """Return a slice of shape patchsize X patchsize from img centered on coord."""
+        x, y = coord[0], coord[1]
+        xstart, xstop = x - self.padwidth, x + self.padwidth + 1
+        ystart, ystop = y - self.padwidth, y + self.padwidth + 1
+
+        return mat[xstart:xstop, ystart:ystop, :]
+
     def _propagate(self, even):
         """Propagate adjacent good offsets."""
         # get the offsets
         if even:
             for index, offset in zip(range(self.NNF.size), self.NNF):
-                p, q0, q1, q2 = self._get_patches(index, offset)
+                p, q0, q1, q2 = self._get_patch_centers(index, offset)
         else:
             for index, offset in zip(range(self.NNF.size-1, -1, -1), self.NNF[::-1]):
-                p, q0, q1, q2 = self._get_patches(index, offset, scan_order=False)
+                p, q0, q1, q2 = self._get_patch_centers(index, offset, scan_order=False)
 
             # compute distance D between:
             # A[x, y], B[f(x, y)], B[f(x-1, y) + (1, 0)], and B[f(x, y-1) + (0, 1)]
@@ -158,62 +216,3 @@ class Patchmatcher(object):
             self._propagate(even=even)
             self._random_search()
 
-    def  bidirectional_distance(self, p, q):
-        """Return the bidirectionally weighted distance D between patches P and Q.
-
-        Where p and q are patch centers in the source and target images respectively.
-
-        Parameters
-        ----------
-        p: 
-
-        q: 
-
-        A: ndarray
-            The original structural image.
-        Ap: ndarray
-            The analogous, synthetic structural image.
-        B: ndarray
-            The analogous, synthetic style image.
-        Bp: ndarray
-            The original style image.
-
-        Returns
-        ------
-        d: float32
-            the computed distance.
-        """
-
-    def unidirectional_distance(self, p, q):
-        """Return the unidirectional distance D between patches P and Q.
-
-        Where p and q are patch centers in the source and target images respectively.
-
-        Parameters
-        ----------
-        p: patch center from image A
-
-        q: patch center from image Bp
-
-        A: ndarray
-            The original structural image.
-        Bp: ndarray
-            The original style image.
-
-        Returns
-        ------
-        d: float32
-            the computed distance.
-        """
-        Fbar = lambda mat: mat / np.linalg.det(mat)
-
-        pstart, pstop = p[0], p[1] + self.padwidth + 1
-        qstart, qstop = q[0], q[1] + self.padwidth + 1
-
-        x = self.A[pstart:pstop, pstart:pstop, :]
-        y = self.Bp[qstart:qstop, qstart:qstop, :]
-
-        fbar_x = Fbar(x)
-        fbar_y = Fbar(y)
-
-        return np.linalg.norm(fbar_x - fbar_y)**2
